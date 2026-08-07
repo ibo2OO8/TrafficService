28e0b38815b6fbe8915fb10f099c758e6023b526
Ibrohim Usmonov <ibrohimusmonov365@gmail.com>
2026-08-07 09:40:41 +0500

    screens are ready

---
+++ app/src/main/java/com/ibrohimapk3/trafficservice/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.ibrohimapk3.trafficservice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.ibrohimapk3.trafficservice.ui.theme.TrafficServiceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                scrim = android.graphics.Color.WHITE, darkScrim = android.graphics.Color.WHITE
+            )
+        )
+        super.onCreate(savedInstanceState)
+        setContent {
+            TrafficServiceTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ServiceScreen(
+                        modifier = Modifier
+                            .padding(top = innerPadding.calculateTopPadding())
+                            .background(Color.White)
+                    )
+                }
+            }
+        }
+    }
+}
